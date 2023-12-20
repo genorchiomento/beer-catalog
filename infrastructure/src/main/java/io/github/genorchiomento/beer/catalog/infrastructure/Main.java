72c55840043d4b15566f7795012063173a7e1ed6
Genor Chiomento <genor.chiomento@gmail.com>
2023-12-20 13:41:53 -0300

    add springboot in project

--- infrastructure/src/main/java/io/github/genorchiomento/beer/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/io/github/genorchiomento/beer/catalog/infrastructure/Main.java
@@ -1,9 +1,13 @@
 package io.github.genorchiomento.beer.catalog.infrastructure;
 
-import io.github.genorchiomento.beer.catalog.application.UseCase;
+import io.github.genorchiomento.beer.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
