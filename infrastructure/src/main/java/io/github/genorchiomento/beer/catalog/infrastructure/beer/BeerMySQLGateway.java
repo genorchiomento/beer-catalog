c38e9a79209b6aa76e169f6657b50450e43a763b
Genor Chiomento <genor.chiomento@gmail.com>
2023-12-28 15:26:46 -0300

    add implementation beer gateway to create

--- infrastructure/src/main/java/io/github/genorchiomento/beer/catalog/infrastructure/beer/BeerMySQLGateway.java
+++ infrastructure/src/main/java/io/github/genorchiomento/beer/catalog/infrastructure/beer/BeerMySQLGateway.java
@@ -5,6 +5,7 @@ import io.github.genorchiomento.beer.catalog.domain.beer.BeerGateway;
 import io.github.genorchiomento.beer.catalog.domain.beer.BeerID;
 import io.github.genorchiomento.beer.catalog.domain.beer.BeerSearchQuery;
 import io.github.genorchiomento.beer.catalog.domain.pagination.Pagination;
+import io.github.genorchiomento.beer.catalog.infrastructure.beer.persistence.BeerJpaEntity;
 import io.github.genorchiomento.beer.catalog.infrastructure.beer.persistence.BeerRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class BeerMySQLGateway implements BeerGateway {
 
     @Override
     public Beer create(Beer aBeer) {
-        return null;
+        return repository.save(BeerJpaEntity.from(aBeer)).toAggregate();
     }
 
     @Override
