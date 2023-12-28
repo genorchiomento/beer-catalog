26753cf8322dbfbfb6899123e3eafc42c684e760
Genor Chiomento <genor.chiomento@gmail.com>
2023-12-28 13:14:38 -0300

    remove implementation beer jpa repository test in main

--- infrastructure/src/main/java/io/github/genorchiomento/beer/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/io/github/genorchiomento/beer/catalog/infrastructure/Main.java
@@ -1,18 +1,9 @@
 package io.github.genorchiomento.beer.catalog.infrastructure;
 
-import io.github.genorchiomento.beer.catalog.domain.beer.Beer;
-import io.github.genorchiomento.beer.catalog.domain.beer.enumerable.ColorEnum;
-import io.github.genorchiomento.beer.catalog.domain.beer.enumerable.StyleEnum;
-import io.github.genorchiomento.beer.catalog.infrastructure.beer.persistence.BeerJpaEntity;
-import io.github.genorchiomento.beer.catalog.infrastructure.beer.persistence.BeerRepository;
 import io.github.genorchiomento.beer.catalog.infrastructure.configuration.WebServerConfig;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -23,30 +14,5 @@ public class Main {
                 "development"
         );
         SpringApplication.run(WebServerConfig.class, args);
-    }
-
-    @Bean
-    public ApplicationRunner runner(BeerRepository beerRepository) {
-        return args -> {
-
-            List<BeerJpaEntity> all = beerRepository.findAll();
-
-            Beer beer = Beer.newBeer(
-                    "Brahma",
-                    StyleEnum.ALE,
-                    "Brasil",
-                    50.0,
-                    6.6,
-                    ColorEnum.CLARA,
-                    "",
-                    "",
-                    "",
-                    true
-            );
-
-            beerRepository.saveAndFlush(BeerJpaEntity.from(beer));
-
-            beerRepository.deleteAll();
-        };
     }
 }
