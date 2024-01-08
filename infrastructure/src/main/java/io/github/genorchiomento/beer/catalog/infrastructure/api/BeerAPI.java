2429cbd2d91aaff217de13fa33a7c33b1ba1a643
Genor Chiomento <genor.chiomento@gmail.com>
2024-01-08 15:43:28 -0300

    add operations and api responses in swagger

--- infrastructure/src/main/java/io/github/genorchiomento/beer/catalog/infrastructure/api/BeerAPI.java
+++ infrastructure/src/main/java/io/github/genorchiomento/beer/catalog/infrastructure/api/BeerAPI.java
@@ -1,6 +1,10 @@
 package io.github.genorchiomento.beer.catalog.infrastructure.api;
 
 import io.github.genorchiomento.beer.catalog.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +13,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping(value = "beers")
+@Tag(name = "Beers")
 public interface BeerAPI {
 
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Create a new Beer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created Successfully"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+
+    })
     ResponseEntity<?> createBeer();
 
     @GetMapping
+    @Operation(summary = "List all beers paginated")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Listed Successfully"),
+            @ApiResponse(responseCode = "422", description = "A invalid parameter was received"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+
+    })
     Pagination<?> listBeers(
             @RequestParam(name = "search", required = false, defaultValue = "") final String search,
             @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
