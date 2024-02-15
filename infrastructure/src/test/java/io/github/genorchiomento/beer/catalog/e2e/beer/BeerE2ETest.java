92699d7acd179c497d1ecf8c8c41526efbe7bfbb
Genor Chiomento <genor.chiomento@gmail.com>
2024-02-15 11:37:48 -0300

    add dependencies to e2e test

---
+++ infrastructure/src/test/java/io/github/genorchiomento/beer/catalog/e2e/beer/BeerE2ETest.java
@@ -0,0 +1,31 @@
+package io.github.genorchiomento.beer.catalog.e2e.beer;
+
+import io.github.genorchiomento.beer.catalog.E2ETest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@E2ETest
+@Testcontainers
+public class BeerE2ETest {
+
+    @Container
+    private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:latest")
+            .withPassword("123456")
+            .withUsername("root")
+            .withDatabaseName("adm_beer");
+
+    @DynamicPropertySource
+    public static void setDatasourceProperties(final DynamicPropertyRegistry registry) {
+        registry.add("mysql.port", () -> MYSQL_CONTAINER.getMappedPort(3306));
+    }
+
+    @Test
+    public void testWorks() {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+    }
+}
